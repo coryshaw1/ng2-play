067c8a5b226f301eef9f9cac2606b47ebcb2ec3b
coryshaw1 <cory.shaw.dev@gmail.com>
2016-05-06 17:42:09 -0400

    Fix breaking changes to date picker

--- src/hello.ts
+++ src/hello.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {DateRangePicker, Collapse} from 'fuel-ui/fuel-ui';
+import {DATE_PICKER_PROVIDERS, DateRange, Collapse} from 'fuel-ui/fuel-ui';
 
 @Component({
     selector: 'hello-app',
@@ -8,8 +8,28 @@ import {DateRangePicker, Collapse} from 'fuel-ui/fuel-ui';
         <h1>Hello, {{name}}!</h1>
         Say hello to: <input [value]="name" (input)="name = $event.target.value">
         <div class="row">
-            <div class="col-md-4">
-                <date-range-picker></date-range-picker>
+            <div class="col-md-6">
+                <form class="form-inline">
+                    <date-range-picker
+                        minDate="11/1/2015"
+                        maxDate="11/12/2016" 
+                        [dateFilter]="dateFilter"
+                        (valueChange)="datePickerValueChange($event)">
+                        <div class="form-group">
+                            <label for="arrival">Arrival Date</label>
+                            <div class="date-picker-input-group">
+                                <input name="arrival" startDateField class="form-control" value="5/5/2016" placeholder="Arrival" />
+                            </div>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="departure">Departure Date</label>
+                            <div class="date-picker-input-group">
+                                <input name="departure" endDateField class="form-control" value="5/10/2016" placeholder="Departure" />
+                            </div>
+                        </div>
+                    </date-range-picker>
+                </form>
             </div>
         </div>
 
@@ -30,12 +50,18 @@ import {DateRangePicker, Collapse} from 'fuel-ui/fuel-ui';
             <p>At any time!</p>
         </div>
     `,
-    directives: [DateRangePicker, Collapse]
+    directives: [DATE_PICKER_PROVIDERS, Collapse]
 })
 export class HelloApp {
     name: string = 'World';
     collapsed: boolean = false;
     duration: number = 500;
+    
+    dateRangePickerValue: DateRange;
+    
+    datePickerValueChange(eventValue: any){
+        this.dateRangePickerValue = eventValue;
+    }
 }
 
 bootstrap(HelloApp);
